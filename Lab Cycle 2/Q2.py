dae6608fff87a0bc6698f2394a254b0121c4082c
A_Nama <145112688+A-Nama@users.noreply.github.com>
2025-02-18 14:58:06 +0530

    Completed answer.py

---
+++ Lab Cycle 2/Q2.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_eoq(demand, ordering_cost, holding_cost):
+    return math.sqrt((2*demand*ordering_cost)/holding_cost)
+
+annual_demand = 10000
+ordering_cost = 200
+holding_cost = 5
+eoq = calculate_eoq(annual_demand, ordering_cost, holding_cost)
+
+print(f"The Economic Order Quantity (EOQ) is : {eoq: .2f}")
